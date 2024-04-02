c07c03badb0ad098f9738912f5428ca95264d816
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2024-04-02 17:19:05 +0200

    set up all CI/CD Pipelines for Software Quality

---
+++ src/main/java/com/bohdanvlad/Main.java
@@ -0,0 +1,14 @@
+package com.bohdanvlad;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+
+    public static int testMaven()
+    {
+        return 2;
+    }
+}
---
+++ src/test/java/com/bohdanvlad/MainTest.java
@@ -0,0 +1,15 @@
+package com.bohdanvlad;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest
+{
+
+    @Test
+    void testMaven()
+    {
+        assertEquals(2, Main.testMaven());
+    }
+}
