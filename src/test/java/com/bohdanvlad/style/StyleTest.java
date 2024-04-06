f4ef14f6f3850babf08fc0dabc4c75158b0896b4
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2024-04-07 00:20:41 +0200

    added tests

---
+++ src/test/java/com/bohdanvlad/style/StyleTest.java
@@ -0,0 +1,15 @@
+package com.bohdanvlad.style;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StyleTest
+{
+
+    @Test
+    void getIndent()
+    {
+        //create normal tests
+    }
+}
